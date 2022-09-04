c5e9bf83c46f168f39f19b950c89555ed42a6538
Daniel JCH <danielj.callisayah@gmail.com>
2022-09-04 11:41:26 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
